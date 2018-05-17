f3ecfcfba76706a14fae76f379cf19acfe9289eb
Chad Hurd <churd@obsglobal.com>
2018-05-17 13:24:19 -0500

    initial sql migration

---
+++ webapi/database/Migrations/201805171823099_initial.cs
@@ -0,0 +1,27 @@
+namespace database.Migrations
+{
+    using System;
+    using System.Data.Entity.Migrations;
+    
+    public partial class initial : DbMigration
+    {
+        public override void Up()
+        {
+            CreateTable(
+                "dbo.Logs",
+                c => new
+                    {
+                        Id = c.Int(nullable: false, identity: true),
+                        Time = c.DateTime(nullable: false),
+                        Message = c.String(),
+                    })
+                .PrimaryKey(t => t.Id);
+            
+        }
+        
+        public override void Down()
+        {
+            DropTable("dbo.Logs");
+        }
+    }
+}
